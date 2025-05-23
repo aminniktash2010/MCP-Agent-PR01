c0d412ce089e589b8485d10013de4b4014cc35ec
MCP Agent <aniktash@amptech.ca>
2025-05-22 23:20:51 -0400

    22

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ provider "azurerm" {
   features {}
   
   # These will be picked up from environment variables:
-  # ARM_CLIENT_ID
+  # ARM_CLIENT_ID1
   # ARM_CLIENT_SECRET
   # ARM_SUBSCRIPTION_ID
   # ARM_TENANT_ID
